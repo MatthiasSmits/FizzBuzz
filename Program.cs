32b6560c3a09ca0eb25dfebb0380a95f180b4597
MatthiasSmits <35918374+MatthiasSmits@users.noreply.github.com>
2018-01-29 11:11:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string ans = "";
+                if (i % 3 == 0)
+                    ans += "Fizz";
+                if (i % 5 == 0)
+                    ans += "Buzz";
+                if (ans == "")
+                    ans = i.ToString();
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
